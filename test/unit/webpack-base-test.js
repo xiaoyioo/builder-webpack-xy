f13cf9ab2cfa427e9eee7e8bbba2c5b65814e3af
xycao <janexcxy@163.com>
2020-03-02 17:22:00 +0800

    test: webpack-base-test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
 
     it('entry',()=>{
-        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js')>-1, true);
-        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js')>-1, true);
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-xy/test/smoke/template/src/index/index.js')>-1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack-xy/test/smoke/template/src/search/index.js')>-1, true);
     })
 })
